5fd8ad9187edc9f2af940b8f31614eb800f1d08b
Sepyan Kristanto <sepyankristanto@Sepyans-MacBook-Pro.local>
2024-07-17 13:19:21 +0700

    uodate profile activity

--- app/src/main/java/poliwangi/ac/id/dtsapp/ProfileActivity.java
+++ app/src/main/java/poliwangi/ac/id/dtsapp/ProfileActivity.java
@@ -7,9 +7,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import poliwangi.ac.id.dtsapp.data.AppDbProvider;
+import poliwangi.ac.id.dtsapp.data.User;
+import poliwangi.ac.id.dtsapp.data.UserDao;
+
 
 public class ProfileActivity extends AppCompatActivity
 {
+    private User currentUser;
     // Komponen
     private EditText edtUsername;
     private EditText edtPassword;
@@ -28,6 +33,11 @@ public class ProfileActivity extends AppCompatActivity
 
     private void loadData()
     {
+        // Mendatapatkan DAO dari DTSAppDatabase
+        UserDao daoUser = AppDbProvider.getInstance(this).userDao();
+
+        // Melakukan SELECT terhadap 1 user yang paling awal, dan mengembalikan hasilnya ke property currentUser
+        this.currentUser = daoUser.selectOne();
 
     }
 
@@ -38,6 +48,21 @@ public class ProfileActivity extends AppCompatActivity
         this.edtEmail = this.findViewById(R.id.edt_email);
         this.edtPhoneNumber = this.findViewById(R.id.edt_phone_number);
         this.btnSave = this.findViewById(R.id.btn_save);
+
+
+
+        // Jika tidak ada data registrasi sebelumnya, tidak perlu melakukan apa-apa, dan matikan Button Save agar user tidak menyimpan data kosong.
+        if(this.currentUser == null)
+        {
+            this.btnSave.setEnabled(false);
+            return;
+        }
+
+        // Menyalin data dari property currentUser ke semua komponen yang sesuai
+        this.edtUsername.setText(this.currentUser.username);
+        this.edtPassword.setText(this.currentUser.password);
+        this.edtEmail.setText(this.currentUser.email);
+        this.edtPhoneNumber.setText(this.currentUser.phoneNumber);
     }
 
     public void onBtnSave_Click(View view)
@@ -48,5 +73,14 @@ public class ProfileActivity extends AppCompatActivity
     public void onTxvDeleteAccount_Click(View view)
     {
         Toast.makeText(this, "Your data has been deleted..", Toast.LENGTH_SHORT).show();
+    }
+
+
+    // Menangkap data dari semua EditText dan menyalinnya ke Entity currentUser
+    private void syncData()
+    {
+        this.currentUser.password = this.edtPassword.getText().toString();
+        this.currentUser.email = this.edtEmail.getText().toString();
+        this.currentUser.phoneNumber = this.edtPhoneNumber.getText().toString();
     }
 }
